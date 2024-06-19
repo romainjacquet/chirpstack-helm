658b7177421b981484a3d2b1f29a662749bfc628
ArienKN <adrien.knobloch@gmail.com>
2024-06-19 15:48:05 +0200

    Update stylesheet KalisioMaps

--- charts/chirpstack/config_kano/dev-layers.cjs
+++ charts/chirpstack/config_kano/dev-layers.cjs
@@ -73,232 +73,285 @@ module.exports = [
     }
   },
   {
-    name: 'Layers.CHIRPSTACK_BATTERY',
-    description: 'Layers.CHIRPSTACK_BATTERY_DESCRIPTION',
+    name: 'Layers.LORAWAN_GATEWAY',
+    description: 'Layers.LORAWAN_GATEWAY_DESCRIPTION',
     i18n: {
       fr: {
         Layers: {
-          CHIRPSTACK_BATTERY: 'batterie LoRaWAN',
-          CHIRPSTACK_BATTERY_DESCRIPTION: 'Capteurs LORAWAN du démonstrateur Kalisio, infos communes'
+          LORAWAN_GATEWAY: 'Gateways',
+          LORAWAN_GATEWAY_DESCRIPTION: 'Gateways LORAWAN du démonstrateur Kalisio',
         },
-        Variables: {
-          LORAWAN_SENSORS_LABEL:'capteurs de mesures LoRaWAN',
-          BATTERY_LEVEL_LABEL: 'Taux de charge batterie'
+        Legend: {
+          LORAWAN_GATEWAY_LABEL: 'LoRaData - Gateways',
+          LORAWAN_GATEWAY: 'Gateways'
         }        
       },
       en: {
         Layers: {
-          CHIRPSTACK_BATTERY: 'batterie LoRaWAN',
-          CHIRPSTACK_BATTERY_DESCRIPTION: 'LoRaWAN sensors of the demonstrator, common values'
-        },    
-        Variables: {
-          LORAWAN_SENSORS_LABEL:'LoRaWAN sensors values',
-          BATTERY_LEVEL_LABEL: 'Batterie level'
-        }    
-      }
-    },
-    tags: ['sensors'],
-    type: 'OverlayLayer',
-    service: 'chirpstack-observations',
-    probeService: 'chirpstack-stations',
-    ttl: 7 * 24 * 60 * 60,
-    featureId: 'gw_euid',
-    featureLabel: 'Variables.LORAWAN_SENSORS_LABEL',
-    from: 'P-0D',
-    to: 'PT-5D',
-    every: 'PT1H',
-    queryFrom: 'PT-10M',
-    variables: [
-      {
-        name: 'battery',
-        label: 'Variables.BATTERY_LEVEL_LABEL',
-        unit: ['%'],
-        range: [0, 100],
-        step: 5,
-        chartjs: {
-          backgroundColor: 'rgba(1, 10, 1, 128)',
-          borderColor: 'rgb(1, 10, 1)',
-          fill: false
+          LORAWAN_GATEWAY: 'Gateways',
+          LORAWAN_GATEWAY_DESCRIPTION: 'LoRaWAN Gateways of the demonstrator',
+        },
+        Legend: {
+          LORAWAN_GATEWAY_LABEL: 'LoRaData - Gateways',
+          LORAWAN_GATEWAY: 'Gateways'
         }
       }
-    ],
+    },
+    legend: [{
+      type: 'variables',
+      label: 'Legend.LORAWAN_GATEWAY_LABEL',
+      minZoom: 6,
+    }, {
+      type: 'symbols',
+      minZoom: 8,
+      content: {
+        gateways: [{
+          symbol: { 'media/KShape': { options: { shape: 'circle', color: 'white', radius: 10, icon: { classes: 'fa fa-wifi', color: 'black',  size: 10}, stroke: { color: 'black', size: 2 } } } }, 
+          label: 'Legend.LORAWAN_GATEWAY' 
+        }]
+      }
+    }],
+    attribution: "<a href='https://www.chirpstack.io/'>ChirStack</a>",
+    tags: ['sensors'],
+    type: 'OverlayLayer',
+    service: 'chirpstack-stations',
+    featureId: 'gw_euid',
     leaflet: {
       type: 'geoJson',
       realtime: true,
       tiled: true,
-      cluster: { disableClusteringAtZoom: 18 },
-      'marker-color': `<% if (properties.status === 'OK') { %>green<% }
-                          else { %>red<% } %>`,
-      'icon-classes': 'fa fa-wifi',
-      'icon-x-offset': -2,
-      'icon-color': '#FFF',
-      template: ['marker-color'],
+      minZoom: 6,
+      cluster: { 
+        maxClusterRadius: 28,
+        disableClusteringAtZoom: 18 
+      },
+      source: '/api/chirpstack-stations',
+      style: {
+        point: {
+          shape: 'circle',
+          radius: 15,
+          opacity: 1,
+          color: `<% %>white<% %>`,
+          stroke: {
+            color: `<% %>black<% %>`,
+            width: 2
+          },
+          icon: {
+            color: `<% %>black<% %>`,
+            classes: 'fa fa-wifi',
+          }
+        }
+      },       
+      template: ['style.point.color', 'style.point.stroke.color', 'style.point.icon.color'],
       tooltip: {
-        template: 'Gateway: <%= properties.euid %>'
+        template: `<%= properties.name %>`
       }
     }
-  },
+  }, 
   {
-    name: 'Layers.CHIRPSTACK_TEMPERATURE',
-    description: 'Layers.CHIRPSTACK_DESCRIPTION',
-    i18n: {
-      fr: {
-        Layers: {
-          CHIRPSTACK_TEMPERATURE: 'Température',
-          CHIRPSTACK_DESCRIPTION: 'Température des capteurs LORAWAN'
-        },
-        Variables: {         
-          LORAWAN_SENSORS_LABEL:'capteurs de mesures LoRaWAN',
-          TEMPERATURE_LABEL: 'Température',          
-        }        
+  name: 'Layers.LORAWAN',
+  description: 'Layers.LORAWAN_DESCRIPTION',
+  i18n: {
+    fr: {
+      Layers: {
+        LORAWAN: 'Capteurs par Application',
+        LORAWAN_DESCRIPTION: 'Capteurs LORAWAN du démonstrateur Kalisio, triés par Application',
+        APP1: 'Capteurs chez Adrien',
+        APP2: 'Capteurs chez Romain'
       },
-      en: {
-        Layers: {
-          CHIRPSTACK_TEMPERATURE: 'Temperature',
-          CHIRPSTACK_DESCRIPTION: 'Temperature of LORAWAN sensors'
-        },    
-        Variables: {
-          LORAWAN_SENSORS_LABEL:'LoRaWAN sensors values',
-          TEMPERATURE_LABEL: 'Temperature'
-        }    
-      }
+      Legend: {
+        LORAWAN_MEASUREMENTS_LABEL: 'LoRaData - Mesures',
+        LORAWAN_SENSORS_LABEL: 'LoRaData - Capteurs',
+        LORAWAN_MEASUREMENT: 'Dernières mesures',
+        LORAWAN_SENSOR: 'Capteurs'
+      },
+      Variables: {         
+        LORAWAN_SENSORS_LABEL:'Capteurs de mesures LoRaWAN',
+        TEMPERATURE_LABEL: 'Température',
+        HUMIDITY_LABEL: `Taux d'humidité`,
+        BATTERY_LEVEL_LABEL: 'Taux de charge batterie',
+        LAEQ: 'Niveau de pression acoustique continue',
+        LAF: 'Niveau sonore avec Pondération Fréquentielle',
+        LAFMAX: 'Niveau Sonore Maximal avec Pondération Fréquentielle ' 
+      }        
     },
-    tags: ['sensors'],
-    type: 'OverlayLayer',
-    service: 'chirpstack-observations',
-    probeService: 'chirpstack-stations',
-    ttl: 7 * 24 * 60 * 60,
-    featureId: 'gw_euid',
-    featureLabel: 'Variables.LORAWAN_SENSORS_LABEL',
-    from: 'P-0D',
-    to: 'PT-5D',
-    every: 'PT1H',
-    queryFrom: 'PT-10M',
-    variables: [
-      {
-        name: 'temperature',
-        label: 'Variables.TEMPERATURE_LABEL',
-        unit: ['degC'],
-        range: [-50, 127],
-        step: 1,
-        chartjs: {
-          backgroundColor: 'rgba(255, 99, 132, 128)',
-          borderColor: 'rgb(255, 99, 132)',
-          fill: false
-        }
-      }
-    ],
-    leaflet: {
-      type: 'geoJson',
-      realtime: true,
-      tiled: true,
-      cluster: { disableClusteringAtZoom: 18 },
-      'marker-color': `<% if (properties.status === 'OK') { %>green<% }
-                          else { %>red<% } %>`,
-      'icon-classes': 'fa fa-wifi',
-      'icon-x-offset': -2,
-      'icon-color': '#FFF',
-      template: ['marker-color'],
-      tooltip: {
-        template: 'Gateway: <%= properties.euid %>'
-      }
+    en: {
+      Layers: {
+        LORAWAN: 'Sensors per Application',
+        LORAWAN_DESCRIPTION: 'LoRaWAN sensors of the demonstrator, sorted per Application',
+        APP1: `Adrien's sensors`,
+        APP2: `Romain's sensors`
+      },
+      Legend: {
+        LORAWAN_MEASUREMENTS_LABEL: 'LoRaData - Measurements',
+        LORAWAN_SENSORS_LABEL: 'LoRaData - Sensors',
+        LORAWAN_MEASUREMENT: 'Last measurements',
+        LORAWAN_SENSOR: 'Sensors'
+      },    
+      Variables: {
+        LORAWAN_SENSORS_LABEL:'LoRaWAN sensors values',
+        TEMPERATURE_LABEL: 'Temperature',
+        HUMIDITY_LABEL: 'Humidity level',
+        BATTERY_LEVEL_LABEL: 'Battery level',
+        LAEQ: 'Sound level A-weighted equivalent',
+        LAF: 'Sound level A-weighted',
+        LAFMAX: 'Sound level A-weighted fast maximum'
+      }    
     }
   },
-  ,
-  {
-    name: 'Layers.CHIRPSTACK_SOUND',
-    description: 'Layers.CHIRPSTACK_DESCRIPTION',
-    i18n: {
-      fr: {
-        Layers: {
-          CHIRPSTACK_SOUND: 'Niveau de son',
-          CHIRPSTACK_DESCRIPTION: 'Capteurs de son du réseau LORAWAN'
-        },
-        Variables: {
-          LORAWAN_SENSORS_LABEL:'capteurs de mesures LoRaWAN',
-          LAEQ: 'Niveau de pression acoustique continue',
-          LAF: 'Niveau sonore avec Pondération Fréquentielle',
-          LAFMAX: 'Niveau Sonore Maximal avec Pondération Fréquentielle '       
-        }        
+  legend: [{
+    type: 'variables',
+    label: 'Legend.LORAWAN_MEASUREMENTS_LABEL',
+    minZoom: 8,
+  }, {
+    type: 'symbols',
+    minZoom: 8,
+    content: {
+      measurements: [
+        { symbol: { 'media/KShape': { options: { shape: 'circle', color: '#00a9ce', radius: 10, icon: { classes: 'fa fa-wifi', color: 'white',  size: 10} } } }, 
+        label: 'Legend.LORAWAN_MEASUREMENT' 
       },
-      en: {
-        Layers: {          
-          CHIRPSTACK_SOUND: 'Sound level',
-          CHIRPSTACK_DESCRIPTION: 'LoRaWAN sound sensors'
-        },    
-        Variables: {
-          LORAWAN_SENSORS_LABEL:'LoRaWAN sensors values',
-          LAEQ: 'Sound level A-weighted equivalent',
-          LAF: 'Sound level A-weighted',
-          LAFMAX: 'Sound level A-weighted fast maximum'   
-        }    
+        /*{ symbol: { 'media/KShape': { options: { shape: 'circle', color: 'black', radius: 10, icon: { classes: 'fa fa-wifi', color: 'white', size: 10 } } } }, 
+          label: 'Legend.LORAWAN_OLD_MEASUREMENT' 
+        }*/
+      ]
+    }
+  }],
+  attribution: "<a href='https://www.chirpstack.io/'>ChirStack</a>",
+  tags: ['sensors'],
+  type: 'OverlayLayer',
+  service: 'chirpstack-observations',
+  featureId: 'euid',
+  featureLabel: 'name',
+  ttl: 7 * 24 * 60 * 60,
+  from: 'P-7D',
+  to: 'PT-1H',
+  every: 'PT1H',
+  queryFrom: 'PT-24H',
+  variables: [
+    {
+      name: 'temperature',
+      label: 'Variables.TEMPERATURE_LABEL',
+      unit: ['°C'],
+      range: [-50, 127],
+      step: 1,
+      chartjs: {
+        backgroundColor: 'rgba(255, 99, 132, 128)',
+        borderColor: 'rgb(255, 99, 132)',
+        fill: false
       }
     },
-    tags: ['sensors'],
-    type: 'OverlayLayer',
-    service: 'chirpstack-observations',
-    probeService: 'chirpstack-stations',
-    ttl: 7 * 24 * 60 * 60,
-    featureId: 'gw_euid',
-    featureLabel: 'Variables.LORAWAN_SENSORS_LABEL',
-    from: 'P-0D',
-    to: 'PT-5D',
-    every: 'PT1H',
-    queryFrom: 'PT-10M',
-    variables: [
-      {
-        name: 'lamax',
-        label: 'Variables.LAFMAX',
-        unit: ['dbA'],
-        range: [0, 160],
-        step: 1,
-        chartjs: {
-          backgroundColor: 'rgba(79, 56, 0, 128)',
-          borderColor: 'rgb(79, 56, 0)',
-          fill: false
-        }
-      },
-      {
-        name: 'laeq',
-        label: 'Variables.LAEQ',
-        unit: ['dbA'],
-        range: [0, 160],
-        step: 1,
-        chartjs: {
-          backgroundColor: 'rgba(79, 14, 0, 128)',
-          borderColor: 'rgb(79, 14, 0)',
-          fill: false
-        }
-      },
-      {
-        name: 'la',
-        label: 'Variables.LAF',
-        unit: ['dbA'],
-        range: [0, 160],
-        step: 1,
-        chartjs: {
-          backgroundColor: 'rgba(79, 0, 35, 128)',
-          borderColor: 'rgb(79, 0, 35)',
-          fill: false
+    {
+      name: 'humidity',
+      label: 'Variables.HUMIDITY_LABEL',
+      unit: ['%'],
+      range: [-50, 127],
+      step: 1,
+      chartjs: {
+        backgroundColor: 'rgb(70, 137, 253, 128)',
+        borderColor: 'rgb(70, 137, 253)',
+        fill: false
+      }
+    },
+    {
+      name: 'battery',
+      label: 'Variables.BATTERY_LEVEL_LABEL',
+      unit: ['%'],
+      range: [0, 100],
+      step: 5,
+      chartjs: {
+        backgroundColor: 'rgba(1, 10, 1, 128)',
+        borderColor: 'rgb(1, 10, 1)',
+        fill: false
+      }
+    },
+    {
+      name: 'lamax',
+      label: 'Variables.LAFMAX',
+      unit: ['dbA'],
+      range: [0, 160],
+      step: 1,
+      chartjs: {
+        backgroundColor: 'rgba(79, 56, 0, 128)',
+        borderColor: 'rgb(79, 56, 0)',
+        fill: false
+      }
+    },
+    {
+      name: 'laeq',
+      label: 'Variables.LAEQ',
+      unit: ['dbA'],
+      range: [0, 160],
+      step: 1,
+      chartjs: {
+        backgroundColor: 'rgba(79, 14, 0, 128)',
+        borderColor: 'rgb(79, 14, 0)',
+        fill: false
+      }
+    },
+    {
+      name: 'la',
+      label: 'Variables.LAF',
+      unit: ['dbA'],
+      range: [0, 160],
+      step: 1,
+      chartjs: {
+        backgroundColor: 'rgba(79, 0, 35, 128)',
+        borderColor: 'rgb(79, 0, 35)',
+        fill: false
+      }
+    }
+  ],
+  filters: [{
+    label: 'Layers.APP1',
+    isActive: true,
+    active: { 'properties.app_name': 'test' },
+    inactive: {}
+  }, {
+    label: 'Layers.APP2',
+    isActive: false,
+    active: { 'properties.app_name': 'KalisioMap' },
+    inactive: {}
+  }],
+  leaflet: {
+    type: 'geoJson',
+    realtime: true,
+    tiled: true,
+    minZoom: 8,
+    cluster: { 
+      maxClusterRadius: 28,
+      disableClusteringAtZoom: 18 
+    },
+    source: '/api/chirpstack-observations',
+    style: {
+      point: {
+        shape: 'circle',
+        radius: 15,
+        opacity: 1,
+        color: `<% if (_.has(properties, 'name')) { %>#138dce<% }
+                    else if (feature.measureRequestIssued) { %>black<% }
+                    else { %>white<% } %>`,
+        stroke: {
+          color: `<% if (_.has(properties, 'name')) { %>transparent<% }
+                    else if (feature.measureRequestIssued) { %>white<% }
+                    else { %>black<% } %>`,
+          width: 2
+        },
+        icon: {
+          color: `<% if (_.has(properties, 'name')) { %>white<% }
+                    else if (feature.measureRequestIssued) { %>white<% }
+                    else { %>black<% } %>`,
+          classes: 'fa fa-wifi',
         }
       }
-    ],
-    leaflet: {
-      type: 'geoJson',
-      realtime: true,
-      tiled: true,
-      cluster: { disableClusteringAtZoom: 18 },
-      'marker-color': `<% if (properties.status === 'OK') { %>green<% }
-                          else { %>red<% } %>`,
-      'icon-classes': 'fa fa-wifi',
-      'icon-x-offset': -2,
-      'icon-color': '#FFF',
-      template: ['marker-color'],
-      tooltip: {
-        template: 'Sensor: <%= properties.euid %>'
-      }
+    },       
+    template: ['style.point.color', 'style.point.stroke.color', 'style.point.icon.color'],
+    tooltip: {
+      template: `<%= properties.name %>`
     }
   }
+}
 ]
+
 
 
